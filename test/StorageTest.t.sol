79869c300d8b093e42f4a1efccaab5f263757503
Luke Sheppard <sheppiegit@gmail.com>
2023-07-15 18:26:18 -0400

    Added a test to check the mappings are stored correctly at the right slots.

--- test/StorageTest.t.sol
+++ test/StorageTest.t.sol
@@ -13,11 +13,28 @@ contract StorageTest is Test {
         storageContract = new Storage();
     }
 
-    function test_StorageSlots() public {
+    function test_IntegerStorageSlots() public {
         uint256 xSlot = stdstore.target(address(storageContract)).sig("numberX()").find();
         uint256 ySlot = stdstore.target(address(storageContract)).sig("numberY()").find();
 
         assertEq(xSlot, 0);
         assertEq(ySlot, 1);
+    }
+
+    function test_MappingStorageSlots() public {
+        uint256 slot1 =
+            stdstore.target(address(storageContract)).sig(storageContract.numMapping.selector).with_key(1).find();
+        uint256 slot31 =
+            stdstore.target(address(storageContract)).sig(storageContract.numMapping.selector).with_key(31).find();
+        uint256 slot41 =
+            stdstore.target(address(storageContract)).sig(storageContract.numMapping.selector).with_key(41).find();
+
+        uint256 atSlot1 = uint256(vm.load(address(storageContract), bytes32(slot1)));
+        uint256 atSlot31 = uint256(vm.load(address(storageContract), bytes32(slot31)));
+
+        uint256 atSlot41 = uint256(vm.load(address(storageContract), bytes32(slot41)));
+        assertEq(atSlot1, 75);
+        assertEq(atSlot31, 98);
+        assertEq(atSlot41, 18);
     }
 }
